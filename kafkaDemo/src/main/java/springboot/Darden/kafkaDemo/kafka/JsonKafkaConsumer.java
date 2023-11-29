4779c5b44b8b863b0e81dc4a5acbb07f583caaf2
rohimukh <rohimukh@INLEN8520050840.corp.capgemini.com>
2023-11-29 12:42:28 +0530

    first commit

---
+++ kafkaDemo/src/main/java/springboot/Darden/kafkaDemo/kafka/JsonKafkaConsumer.java
@@ -0,0 +1,28 @@
+package springboot.Darden.kafkaDemo.kafka;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import springboot.Darden.kafkaDemo.payload.User;
+
+@Service
+public class JsonKafkaConsumer {
+	
+	@Value("${spring.kafka.topic-json.name}")
+	private String topicJsonName;
+	
+	
+	private static final Logger LOGGER=LoggerFactory.getLogger(JsonKafkaConsumer.class);
+@KafkaListener(topics = "${spring.kafka.topic-json.name}",groupId = "${spring.kafka.consumer.group-id}")
+
+public void consume(User user) {
+	
+	LOGGER.info((String.format("json message received-> %s", user.toString())));
+}
+
+}
+
